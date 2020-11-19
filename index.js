f2c29300ababe51354b611166f6d44c31c1144e1
ZhipengLi <charrli@126.com>
2020-11-18 20:28:14 -0800

    java script test

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var person = {
+  firstname:'unknown',
+  lastname:'unknown',
+  getFullName:function(){
+    return this.firstname+ this.lastname;
+  }
+};
+var jane = {
+  firstname:'jane'
+};
+jane.__proto__ = person;
+console.log(jane.getFullName());
